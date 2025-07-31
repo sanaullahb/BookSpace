fd2b67336dd297627670ce7009ff5bca639e8330
sanaullahb <sanaullahb594@gmail.com>
2025-07-30 22:28:52 -0700

    Exclude development files from version control

---
+++ BookSpace/BookSpace/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BookSpace.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BookSpace.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { DisplayOrder = "1", Id = 100, Name ="Action"},
+                new Category { DisplayOrder ="2",Id = 101, Name = "Sci-fi"},
+                new Category { DisplayOrder = "3", Id = 102, Name = "History"}              
+                ); 
+        }
+    }
+}
+
---
+++ BookSpace/BookSpace/Migrations/20250730131621_SeedCategoryTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookSpace.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 100, "1", "Action" },
+                    { 101, "2", "Sci-fi" },
+                    { 102, "3", "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 100);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 102);
+        }
+    }
+}
