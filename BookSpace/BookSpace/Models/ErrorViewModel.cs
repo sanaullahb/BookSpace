89a312e9a52fec081916079e699eef7eb7254d91
sanaullahb <sanaullahb594@gmail.com>
2025-07-30 22:17:50 -0700

    Exclude front-end libs from version control

---
+++ BookSpace/BookSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
